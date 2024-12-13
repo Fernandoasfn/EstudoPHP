b6578b65916d0f14701bbf84dbfed17b4c4dc1b7
Fernando Neves <fernando.asfn@gmail.com>
2024-12-13 11:43:03 -0300

    primeiro commit

---
+++ variaveis.php
@@ -0,0 +1,25 @@
+// VARIAVEIS
+
+// Sempre começam com $.
+// Seguido de letra ou enderline ( _ )
+// NÃO PODEM INICIAR COM NUMEROS.
+
+<?php 
+// Strings:
+
+$nome = 'Fernando';
+$nome2 = "Fernando";
+$_sobrenome = 'Neves';
+
+// inteiro
+$idade = 35;
+
+//float
+$saldoNaConta = 3.50;
+
+//booleano 
+$vontadeDeViver = false;
+$naoAguentoMais = true;
+
+?>
+
